3541df1612928c02d9219e2f5a756922e90741b0
Leo Li <gqy117@gmail.com>
2015-06-20 10:45:31 +0800

    [TwoSum] Added files.

---
+++ Leetcode/Leetcode/000/000/001/TwoSum.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class TwoSum
+    {
+        public static int[] Solution(int[] nums, int target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
