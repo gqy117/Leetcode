08e9153feedec0bda1e3e8b809fbcacab7774a40
Leo Li <gqy117@gmail.com>
2015-06-20 17:34:25 +0800

    [LongestSubstringWithoutRepeatingCharacters] A timeout version.

--- Leetcode/Leetcode/000/000/003/LongestSubstringWithoutRepeatingCharacters.cs
+++ Leetcode/Leetcode/000/000/003/LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,12 +1,41 @@
 ﻿namespace Leetcode
 {
     using System;
+    using System.Collections.Generic;
 
     public class LongestSubstringWithoutRepeatingCharacters
     {
         public static int Solution(string s)
         {
-            throw new NotImplementedException();
+            int n = s.Length;
+            var arrayS = s.ToCharArray();
+            Dictionary<char, bool> distinctChar = new Dictionary<char, bool>();
+            int result = 0;
+            int currentCount = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int k = i;
+
+                while (k < n)
+                {
+                    char currentChar = arrayS[k];
+
+                    if (distinctChar.ContainsKey(currentChar))
+                    {
+                        distinctChar = new Dictionary<char, bool>();
+                        currentCount = 0;
+                        break;
+                    }
+
+                    k++;
+                    currentCount++;
+                    distinctChar.Add(currentChar, true);
+                    result = Math.Max(result, currentCount);
+                }
+            }
+
+            return result;
         }
     }
 }
