f62d1dc44fcefc1477d0b7e0f37e45358ae3ace7
Leo Li <gqy117@gmail.com>
2015-06-20 11:06:51 +0800

    [TwoSum] Accepted.

--- Leetcode/Leetcode/000/000/001/TwoSum.cs
+++ Leetcode/Leetcode/000/000/001/TwoSum.cs
@@ -1,12 +1,47 @@
 ﻿namespace Leetcode
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class TwoSum
     {
         public static int[] Solution(int[] nums, int target)
         {
-            throw new NotImplementedException();
+            int n = nums.Length;
+            IList<SortedNum> sortedNums = new List<SortedNum>();
+
+            for (int i = 0; i < n; i++)
+            {
+                sortedNums.Add(new SortedNum { Index = i, Value = nums[i] });
+            }
+
+            sortedNums = sortedNums.OrderBy(x => x.Value).ToList();
+
+            for (int i = 0;
+                i < sortedNums.Count - 1
+                && sortedNums[i].Value + sortedNums[i + 1].Value <= target;
+                i++)
+            {
+                for (int j = i + 1; j < sortedNums.Count
+                && sortedNums[i].Value + sortedNums[j].Value <= target; j++)
+                {
+                    if (sortedNums[i].Value + sortedNums[j].Value == target)
+                    {
+                        return new int[] { sortedNums[i].Index + 1, sortedNums[j].Index + 1 }.OrderBy(x => x).ToArray();
+                    }
+                }
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
+        private class SortedNum
+        {
+            public int Index { get; set; }
+
+            public int Value { get; set; }
         }
     }
 }
--- Leetcode/LeetcodeTest/000/000/001/TwoSumTest.cs
+++ Leetcode/LeetcodeTest/000/000/001/TwoSumTest.cs
@@ -12,6 +12,18 @@
             this.ExecuteSolution(nums, target, expected);
         }
 
+        [TestCase(new int[] { 3, 2, 4 }, 6, new int[] { 2, 3 })]
+        public void TwoSum_NotSorted(int[] nums, int target, int[] expected)
+        {
+            this.ExecuteSolution(nums, target, expected);
+        }
+
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, -8, new int[] { 3, 5 })]
+        public void TwoSum_NegativeNotSorted(int[] nums, int target, int[] expected)
+        {
+            this.ExecuteSolution(nums, target, expected);
+        }
+
         private void ExecuteSolution(int[] nums, int target, int[] expected)
         {
             // Act
