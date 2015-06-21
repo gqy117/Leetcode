73a01de8f56a8ccd2597f67cfedc093b7cd58900
Leo Li <gqy117@gmail.com>
2015-06-21 18:20:31 +0800

    [PalindromeNumber] Accepted!

--- Leetcode/LeetcodeTest/000/000/009/PalindromeNumberTest.cs
+++ Leetcode/LeetcodeTest/000/000/009/PalindromeNumberTest.cs
@@ -7,12 +7,25 @@
     public class PalindromeNumberTest
     {
         [TestCase(121, true)]
-        public void PalindromeNumber_Default(int x, int expected)
+        public void PalindromeNumber_Default(int x, bool expected)
         {
             this.ExecuteSolution(x, expected);
         }
 
-        private void ExecuteSolution(int x, int expected)
+        [TestCase(-121, false)]
+        public void PalindromeNumber_Negative(int x, bool expected)
+        {
+            this.ExecuteSolution(x, expected);
+        }
+
+        [TestCase(0, true)]
+        [TestCase(1, true)]
+        public void PalindromeNumber_0(int x, bool expected)
+        {
+            this.ExecuteSolution(x, expected);
+        }
+
+        private void ExecuteSolution(int x, bool expected)
         {
             // Act
             var actual = PalindromeNumber.Solution(x);
