f03b820f45a54d5bd16eadfdb4ddea527cac021f
Leo Li <gqy117@gmail.com>
2015-06-21 18:11:11 +0800

    [PalindromeNumber] Added files.

---
+++ Leetcode/Leetcode/000/000/009/PalindromeNumber.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class PalindromeNumber
+    {
+        public static bool Solution(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leetcode/LeetcodeTest/000/000/009/PalindromeNumberTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetcodeTest
+{
+    using Leetcode;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PalindromeNumberTest
+    {
+        [TestCase(121, true)]
+        public void PalindromeNumber_Default(int x, int expected)
+        {
+            this.ExecuteSolution(x, expected);
+        }
+
+        private void ExecuteSolution(int x, int expected)
+        {
+            // Act
+            var actual = PalindromeNumber.Solution(x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
