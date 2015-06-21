d16d87fe1b1d36833817672457c7cab287714048
Leo Li <gqy117@gmail.com>
2015-06-21 20:25:21 +0800

    [RemoveNthNodeFromEndOfList] Accepted.

--- Leetcode/Leetcode/000/010/019/RemoveNthNodeFromEndOfList.cs
+++ Leetcode/Leetcode/000/010/019/RemoveNthNodeFromEndOfList.cs
@@ -1,12 +1,63 @@
 ﻿namespace Leetcode
 {
-    using System;
-
     public class RemoveNthNodeFromEndOfList
     {
         public static ListNode Solution(ListNode head, int n)
         {
-            throw new NotImplementedException();
+            int count = GetCount(head);
+
+            int nodeToBeRemoved = count - n;
+            int nodeBeforeRemoved = nodeToBeRemoved - 1;
+
+            if (IsTryingToRemoveTheFirstNode(nodeToBeRemoved))
+            {
+                return head.next;
+            }
+            else
+            {
+                int i = 0;
+                var theNode = FindNodeBeforeRemoved(head, i, nodeBeforeRemoved);
+                theNode.next = theNode.next.next; 
+            }
+
+            return head;
+        }
+
+        private static ListNode FindNodeBeforeRemoved(ListNode head, int i, int nodeBeforeRemoved)
+        {
+            var subNode = head;
+
+            while (subNode != null)
+            {
+                if (i == nodeBeforeRemoved)
+                {
+                    return subNode;
+                }
+
+                subNode = subNode.next;
+                i++;
+            }
+
+            return null;
+        }
+
+        private static bool IsTryingToRemoveTheFirstNode(int nodeToBeRemoved)
+        {
+            return 0 == nodeToBeRemoved;
+        }
+
+        private static int GetCount(ListNode head)
+        {
+            int count = 0;
+            var subNode = head;
+
+            while (subNode != null)
+            {
+                subNode = subNode.next;
+                count++;
+            }
+
+            return count;
         }
     }
 }
--- Leetcode/LeetcodeTest/000/010/019/RemoveNthNodeFromEndOfListTest.cs
+++ Leetcode/LeetcodeTest/000/010/019/RemoveNthNodeFromEndOfListTest.cs
@@ -1,17 +1,75 @@
 ﻿namespace LeetcodeTest
 {
+    using FluentAssertions;
     using Leetcode;
     using NUnit.Framework;
 
     [TestFixture]
     public class RemoveNthNodeFromEndOfListTest
     {
-        [Test]
-        public void RemoveNthNodeFromEndOfList_Default()
+        [TestCase(2)]
+        public void RemoveNthNodeFromEndOfList_Default(int n)
+        {
+            ListNode head = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                        {
+                            next = new ListNode(5)
+                        }
+                    }
+                }
+            };
+
+            ListNode expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(5)
+                    }
+                }
+            };
+
+            this.ExecuteSolution(head, n, expected);
+        }
+
+        [TestCase(1)]
+        public void RemoveNthNodeFromEndOfList_RemoveLast(int n)
+        {
+            ListNode head = new ListNode(1)
+            {
+                next = new ListNode(2)
+            };
+
+            ListNode expected = new ListNode(1);
+
+            this.ExecuteSolution(head, n, expected);
+        }
+
+        [TestCase(1)]
+        public void RemoveNthNodeFromEndOfList_RemoveOneself(int n)
         {
             ListNode head = new ListNode(1);
-            int n = 1;
+
             ListNode expected = null;
+
+            this.ExecuteSolution(head, n, expected);
+        }
+
+        [TestCase(2)]
+        public void RemoveNthNodeFromEndOfList_RemoveFirst(int n)
+        {
+            ListNode head = new ListNode(1)
+            {
+                next = new ListNode(2)
+            };
+
+            ListNode expected = new ListNode(2);
 
             this.ExecuteSolution(head, n, expected);
         }
@@ -22,7 +80,7 @@
             var actual = RemoveNthNodeFromEndOfList.Solution(head, n);
 
             // Assert
-            Assert.AreEqual(expected, actual);
+            expected.ShouldBeEquivalentTo(actual);
         }
     }
 }
