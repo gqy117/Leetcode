dd945c6618d247cc5b1763c88d5f641d2262c472
Leo Li <gqy117@gmail.com>
2015-06-21 18:30:47 +0800

    [RomanToInteger] Added files.

---
+++ Leetcode/LeetcodeTest/000/010/012/RomanToIntegerTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetcodeTest
+{
+    using Leetcode;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RomanToIntegerTest
+    {
+        [TestCase("V", 10)]
+        public void RomanToInteger_Default(string s, bool expected)
+        {
+            this.ExecuteSolution(s, expected);
+        }
+
+        private void ExecuteSolution(string s, bool expected)
+        {
+            // Act
+            var actual = RomanToInteger.Solution(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
