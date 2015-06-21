6a814b4299c7273c3d02fe7b1a435c206a9db886
Leo Li <gqy117@gmail.com>
2015-06-21 18:56:01 +0800

    [RemoveNthNodeFromEndOfList] Added files.

---
+++ Leetcode/Leetcode/000/010/019/RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class RemoveNthNodeFromEndOfList
+    {
+        public static ListNode Solution(ListNode head, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leetcode/LeetcodeTest/000/010/019/RemoveNthNodeFromEndOfListTest.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetcodeTest
+{
+    using Leetcode;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RemoveNthNodeFromEndOfListTest
+    {
+        [Test]
+        public void RemoveNthNodeFromEndOfList_Default()
+        {
+            ListNode head = new ListNode(1);
+            int n = 1;
+            ListNode expected = null;
+
+            this.ExecuteSolution(head, n, expected);
+        }
+
+        private void ExecuteSolution(ListNode head, int n, ListNode expected)
+        {
+            // Act
+            var actual = RemoveNthNodeFromEndOfList.Solution(head, n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
