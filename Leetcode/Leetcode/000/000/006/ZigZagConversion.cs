5a64a0904b896653df3b3d1813952bb3657edfcf
Leo Li <gqy117@gmail.com>
2015-06-21 16:29:55 +0800

    [ZigZagConversion] Added files.

---
+++ Leetcode/Leetcode/000/000/006/ZigZagConversion.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class ZigZagConversion
+    {
+        public static string Solution(string s, int numRows)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leetcode/LeetcodeTest/000/000/006/ZigZagConversionTest.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetcodeTest
+{
+    using Leetcode;
+    using NUnit.Framework;
+
+    public class ZigZagConversionTest
+    {
+        [TestCase("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        public void ZigZagConversion_Default(string text, int numRows, string expected)
+        {
+            this.ExecuteSolution(text, numRows, expected);
+        }
+
+        private void ExecuteSolution(string text, int numRows, string expected)
+        {
+            // Act
+            var actual = ZigZagConversion.Solution(text, numRows);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
