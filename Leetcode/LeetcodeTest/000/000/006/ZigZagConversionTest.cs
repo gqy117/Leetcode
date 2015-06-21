e5a79b0d07b7b169a651ed2d970aa5a787b572e6
Leo Li <gqy117@gmail.com>
2015-06-21 17:33:21 +0800

    [ZigZagConversion] Wrong answer.

--- Leetcode/LeetcodeTest/000/000/006/ZigZagConversionTest.cs
+++ Leetcode/LeetcodeTest/000/000/006/ZigZagConversionTest.cs
@@ -11,6 +11,19 @@
             this.ExecuteSolution(text, numRows, expected);
         }
 
+        [TestCase("ABC", 2, "ACB")]
+        [TestCase("ABCD", 2, "ACBD")]
+        public void ZigZagConversion_NumRows2(string text, int numRows, string expected)
+        {
+            this.ExecuteSolution(text, numRows, expected);
+        }
+
+        [TestCase("ABCDEFGHIJKL", 5, "AGBHCFILDJEK")]
+        public void ZigZagConversion_NumRows5(string text, int numRows, string expected)
+        {
+            this.ExecuteSolution(text, numRows, expected);
+        }
+
         private void ExecuteSolution(string text, int numRows, string expected)
         {
             // Act
