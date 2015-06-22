8b89478094aab90f6bc6e7c3135b7eb66e638b37
Leo Li <gqy117@gmail.com>
2015-06-22 10:23:41 +0800

    [MergeTwoSortedLists] Accepted.

--- Leetcode/Leetcode/000/020/021/MergeTwoSortedLists.cs
+++ Leetcode/Leetcode/000/020/021/MergeTwoSortedLists.cs
@@ -6,7 +6,45 @@
     {
         public static ListNode Solution(ListNode l1, ListNode l2)
         {
-            throw new NotImplementedException();
+            ListNode result = new ListNode(0);
+            ListNode tempResult = result;
+            ListNode subL1 = l1;
+            ListNode subL2 = l2;
+
+            while (subL1 != null || subL2 != null)
+            {
+                if (GetValue(subL1) < GetValue(subL2))
+                {
+                    SetResultNext(tempResult, subL1);
+                    subL1 = GotoNextNode(subL1);
+                }
+                else
+                {
+                    SetResultNext(tempResult, subL2);
+                    subL2 = GotoNextNode(subL2);
+                }
+
+                tempResult = GotoNextNode(tempResult);
+            }
+
+            return result.next;
+        }
+
+        private static int GetValue(ListNode subListNode)
+        {
+            return subListNode == null ? int.MaxValue : subListNode.val;
+        }
+
+        private static ListNode GotoNextNode(ListNode listNode)
+        {
+            listNode = listNode.next;
+
+            return listNode;
+        }
+
+        private static void SetResultNext(ListNode result, ListNode subList)
+        {
+            result.next = subList;
         }
     }
 }
--- Leetcode/LeetcodeTest/000/020/021/MergeTwoSortedListsTest.cs
+++ Leetcode/LeetcodeTest/000/020/021/MergeTwoSortedListsTest.cs
@@ -34,6 +34,72 @@
             this.ExecuteSolution(l1, l2, expected);
         }
 
+        [Test]
+        public void MergeTwoSortedLists_NotSorted()
+        {
+            ListNode l1 = new ListNode(1)
+            {
+                next = new ListNode(3)
+            };
+
+            ListNode l2 = new ListNode(2)
+            {
+                next = new ListNode(4)
+            };
+
+            ListNode expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    }
+                }
+            };
+
+            this.ExecuteSolution(l1, l2, expected);
+        }
+
+        [Test]
+        public void MergeTwoSortedLists_1()
+        {
+            ListNode l1 = new ListNode(1);
+
+            ListNode l2 = new ListNode(2);
+
+            ListNode expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+            };
+
+            this.ExecuteSolution(l1, l2, expected);
+        }
+
+        [Test]
+        public void MergeTwoSortedLists_l1Null()
+        {
+            ListNode l1 = null;
+
+            ListNode l2 = new ListNode(2);
+
+            ListNode expected = new ListNode(2);
+
+            this.ExecuteSolution(l1, l2, expected);
+        }
+
+        [Test]
+        public void MergeTwoSortedLists_l2Null()
+        {
+            ListNode l1 = new ListNode(2);
+
+            ListNode l2 = null;
+
+            ListNode expected = new ListNode(2);
+
+            this.ExecuteSolution(l1, l2, expected);
+        }
+
         private void ExecuteSolution(ListNode l1, ListNode l2, ListNode expected)
         {
             // Act
