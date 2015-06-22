69a6119b600dd9d6bae2b19ec7d095ddb3ef3f3f
Leo Li <gqy117@gmail.com>
2015-06-22 09:52:51 +0800

    [MergeTwoSortedLists] Added files.

---
+++ Leetcode/Leetcode/000/020/021/MergeTwoSortedLists.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class MergeTwoSortedLists
+    {
+        public static ListNode Solution(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leetcode/LeetcodeTest/000/020/021/MergeTwoSortedListsTest.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetcodeTest
+{
+    using FluentAssertions;
+    using Leetcode;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MergeTwoSortedListsTest
+    {
+        [Test]
+        public void MergeTwoSortedLists_Default()
+        {
+            ListNode l1 = new ListNode(1)
+            {
+                next = new ListNode(2)
+            };
+
+            ListNode l2 = new ListNode(3)
+            {
+                next = new ListNode(4)
+            };
+
+            ListNode expected = new ListNode(1)
+            {
+                next = new ListNode(2)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(4)
+                    }
+                }
+            };
+
+            this.ExecuteSolution(l1, l2, expected);
+        }
+
+        private void ExecuteSolution(ListNode l1, ListNode l2, ListNode expected)
+        {
+            // Act
+            var actual = MergeTwoSortedLists.Solution(l1, l2);
+
+            // Assert
+            expected.ShouldBeEquivalentTo(actual);
+        }
+    }
+}
