6e57332b410caee716198fe7d948747c9d46e069
Leo Li <gqy117@gmail.com>
2015-06-23 20:37:59 +0800

    [LengthOfLastWord] Accepted.

--- Leetcode/Leetcode/000/050/058/LengthOfLastWord.cs
+++ Leetcode/Leetcode/000/050/058/LengthOfLastWord.cs
@@ -1,12 +1,23 @@
 ﻿namespace Leetcode
 {
     using System;
+    using System.Linq;
 
     public class LengthOfLastWord
     {
+        private const string Space = " ";
+
         public static int Solution(string s)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return 0;
+            }
+
+            var splited = s.Split(char.Parse(Space));
+            var lastNonEmpty = splited.Last(x => !string.IsNullOrEmpty(x.Trim()));
+
+            return lastNonEmpty.Length;
         }
     }
 }
--- Leetcode/LeetcodeTest/000/050/058/LengthOfLastWordTest.cs
+++ Leetcode/LeetcodeTest/000/050/058/LengthOfLastWordTest.cs
@@ -12,6 +12,30 @@
             this.ExecuteSolution(s, expected);
         }
 
+        [TestCase("a", 1)]
+        public void LengthOfLastWord_a1(string s, int expected)
+        {
+            this.ExecuteSolution(s, expected);
+        }
+
+        [TestCase("a ", 1)]
+        public void LengthOfLastWord_a1WithSpaceInTheEnd(string s, int expected)
+        {
+            this.ExecuteSolution(s, expected);
+        }
+
+        [TestCase(" a ", 1)]
+        public void LengthOfLastWord_a1WithSpaceInTheEndAndBeginning(string s, int expected)
+        {
+            this.ExecuteSolution(s, expected);
+        }
+
+        [TestCase(" ", 0)]
+        public void LengthOfLastWord_Space(string s, int expected)
+        {
+            this.ExecuteSolution(s, expected);
+        }
+
         private void ExecuteSolution(string s, int expected)
         {
             // Act
