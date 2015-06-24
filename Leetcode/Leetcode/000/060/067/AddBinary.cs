32f875ba4df8b2edde255a6c3e5f882839036277
Leo Li <gqy117@gmail.com>
2015-06-24 20:53:12 +0800

    [AddBinary] Added files.

---
+++ Leetcode/Leetcode/000/060/067/AddBinary.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode
+{
+    using System;
+
+    public class AddBinary
+    {
+        public static string Solution(string a, string b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Leetcode/LeetcodeTest/000/060/067/AddBinaryTest.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetcodeTest
+{
+    using Leetcode;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class AddBinaryTest
+    {
+        [TestCase("11", "1", "100")]
+        public void AddBinary_Default(string a, string b, string expected)
+        {
+            this.ExecuteSolution(a, b, expected);
+        }
+
+        private void ExecuteSolution(string a, string b, string expected)
+        {
+            // Act
+            var actual = AddBinary.Solution(a, b);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
